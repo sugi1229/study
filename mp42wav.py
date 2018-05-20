9f38158df6cede5b5df82b32d003711e19c891ee
msugi <m.sugi1229@gmail.com>
2018-05-21 00:09:26 +0900

    fix

--- mp42wav.py
+++ mp42wav.py
@@ -1,9 +1,10 @@
 import librosa
 import glob
 import stempeg
+import numpy as np
 
 l = glob.glob('./*')
 for i in range(len(l)):
      S, rate = stempeg.read_stems(l[i], stem_id=[0,1,2,3,4])
      A = np.sum(S, axis=0)
-     librosa.output.write_wav(str(i)+'.wav', S[:,0],sr=44100)
+     librosa.output.write_wav(str(i)+'.wav', A[:,0],sr=44100)
